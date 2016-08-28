b023a0463e44e7c046aafaabdd942a60598350c8
黎东潮 <lidc@pv25.com>
2016-08-28 09:37:05 +0800

    yaf框架配置说明

--- framework/yaf.php
+++ framework/yaf.php
@@ -7,5 +7,16 @@
  * memecache  -3.0.8
  * MongoDB-1.4.5
  * yaf-2.2.9
- * nginx-5.4n
+ * php-5.4n
+ * nginx -1.6.2
+ *
+ *
+ * 添加配置  -- 在PHP.ini添加
+ * extension=php_yaf.dll
+ * extension=php_redis.dll
+ * extension=php_memcache.dll
+ * extension=php_mongo.dll
+ *
+ * 在php安装目录里的ext里面放入下载后的配置.dll文件
+ *
  */
