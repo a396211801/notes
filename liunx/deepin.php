0748896551a2d41430c9cefc28b24927c5410894
黎东潮 <lidc@pv25.com>
2017-06-13 14:16:06 +0800

    添加

--- liunx/deepin.php
+++ liunx/deepin.php
@@ -5,3 +5,39 @@
  * Date: 17-6-13
  * Time: 下午2:13
  */
+
+class abc {
+
+    /**
+     * sudo /data/soft/ngx_openresty/nginx/sbin/nginx
+
+    sudo service php5-fpm start
+
+    http://ohmyz.sh/
+
+
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+
+    mysql http://www.cnblogs.com/znan/p/5842125.html
+
+    LDAP 扩展 sudo apt-get install php5-ldap 执行即可
+
+    sudo chmod  -R 777  格莱美领取礼品
+
+    unzip -O GBK crm.zip
+
+    -s reload
+
+
+    ./configure --with-php-config=/usr/bin/php-config
+
+
+    add    /etc/php5/fpm/php.ini  *.so
+     * */
+
+
+}
+
+
