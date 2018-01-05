cfe0b68535e8ce7c6fcef9ea07bc1498fb54cd0c
lidc <396211801@qq.com>
2018-01-05 14:45:22 +0800

    chat

--- server/weChat.php
+++ server/weChat.php
@@ -8,6 +8,19 @@
 class weChat {
 
     /**
+     *
+     * 公众号账号  2863229228@qq.com
+     *
+     * 米饭加盐
+     *
+     * 美食
+     *
+     *  mifanjiayan-chat
+     *
+     * gh_9e6988bc4288
+     *
+     *  AppId   wxed0c997f829f96a2
+     *
      * AppSecret a20170056aec84b3662bc3789638a72a
      *
      * */
